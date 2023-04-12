f7f48917733209f0815defcb28baa496e9ed7ddd
Lewis Prescott <lewis.prescott@ceracare.co.uk>
2023-04-12 21:20:08 +0100

    implement pact cypress adapter

--- cypress/e2e/songs.cy.js
+++ cypress/e2e/songs.cy.js
@@ -1,14 +1,15 @@
 describe('Top songs', () => {
     const url = '/songs/ed-sheeran'
     before(() => {
-        // setup pact
+        // setup pact https://github.com/pactflow/pact-cypress-adapter#cyusepactwaitalias--alias
+        cy.setupPact('ui-top-songs', 'api-top-songs')
         cy.intercept(`${url}*`, { fixture: 'songs.json' }).as('songs')
     })
 
     it('Artist', () => {
         cy.visit('/')
         cy.get('button').click()
-        cy.wait('@songs') // use pact wait
+        cy.usePactWait(['songs']) // use pact wait
         cy.contains('li', 'The A Team').should('be.visible')
     })
 })
